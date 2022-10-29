30b4d0c7ddc9908a3c82722f1a603026789abc52
Beatriz Milz <milz.bea@gmail.com>
2022-10-29 17:37:04 -0300

    workflow with packages

---
+++ scripts/04-r-script.R
@@ -0,0 +1,21 @@
+
+
+library(httr)
+library(fs)
+
+print("Checking which files we have:")
+dir_ls()
+
+#  the URL 
+url_download = "https://app.anm.gov.br/SIGBM/Publico/ClassificacaoNacionalDaBarragem/ExportarExcel"
+
+# file name to save
+file_name = paste0("sigbm_download_", Sys.Date(), ".xlsx")
+
+
+print("Making a POST request and writing file on disk:")
+POST(url_download, write_disk(file_name, overwrite = TRUE))
+
+
+print("Checking which files we have:")
+dir_ls()
